4c9072511b77cc0019ae712ec345a5a9f2592b42
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2022-04-03 04:44:23 +0200

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,5 +7,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'sheerif.abdullah@gmail.com'
-    MAIL_PASSWORD = 'ameame2005'
+    MAIL_USERNAME = 'example@domain.com'
+    MAIL_PASSWORD = 'password'
